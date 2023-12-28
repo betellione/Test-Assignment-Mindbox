9f30cab0b884b0bf3ae0d96372219bb73b09034d
Тимур <timurmiftyakov@gmail.com>
2023-12-28 23:29:17 +0300

    Task 2

---
+++ Task2.sql
@@ -0,0 +1,4 @@
+﻿SELECT p.Name ProductName, c.Name CategoryName
+FROM Product p
+         LEFT JOIN ProductCategory pc ON pc.ProductId = p.Id
+         LEFT JOIN Category c ON pc.CategoryId = c.Id;
---
+++ Task2DbCreation.sql
@@ -0,0 +1,20 @@
+﻿CREATE TABLE Category
+(
+    Id   int PRIMARY KEY IDENTITY,
+    Name nvarchar(255) NOT NULL
+);
+
+CREATE TABLE Product
+(
+    Id   int PRIMARY KEY IDENTITY,
+    Name nvarchar(255) NOT NULL
+);
+
+CREATE TABLE ProductCategory
+(
+    ProductId  int,
+    CategoryId int,
+    PRIMARY KEY (ProductId, CategoryId),
+    FOREIGN KEY (ProductId) REFERENCES Product (Id),
+    FOREIGN KEY (CategoryId) REFERENCES Category (Id)
+);
